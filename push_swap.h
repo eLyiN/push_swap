88fc221157da50ce519d0c23245dc53b3c40b693
ShelakhMadness <shelakh@elyin.com>
2022-06-04 13:44:28 +0200

    first edition

---
+++ push_swap.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aarribas <aarribas@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 10:32:06 by aarribas          #+#    #+#             */
+/*   Updated: 2022/05/31 10:43:55 by aarribas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include "./libft/libft.h"
+
+typedef struct s_stack
+{
+	int				content;
+	struct s_stack	*next;
+}					t_stack;
+
+typedef struct s_global
+{
+	t_stack			*a;
+	t_stack			*b;
+	t_stack			*head_a;
+	t_stack			*head_b;
+}					t_global;
+
+#endif
