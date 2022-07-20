4919f823f33ba4c9586e4825182e408d54d4bebf
Shlk Sobremesa <shelakh@casa.com>
2022-07-20 11:55:57 +0200

    libft updated

--- push_swap.c
+++ push_swap.c
@@ -6,15 +6,22 @@
 /*   By: aarribas <aarribas@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 10:32:10 by aarribas          #+#    #+#             */
-/*   Updated: 2022/07/18 10:01:31 by aarribas         ###   ########.fr       */
+/*   Updated: 2022/07/20 11:54:44 by aarribas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	stacks_fill(int ac, char *av[], t_stack stack_a, t_stack stack_b)
+{
+	stack_a.array = ft_split(av, ' ');
+}
+
 int	main(int ac, char **av)
 {
-	t_global global;
-	if (ac < 2)
-		return (1);
+	t_global	global;
+
+	if (ac < 2 || ac == 1)
+		return (0);
+	stacks_fill(ac, av, *global.a, *global.b);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: aarribas <aarribas@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 10:32:06 by aarribas          #+#    #+#             */
-/*   Updated: 2022/07/18 10:02:41 by aarribas         ###   ########.fr       */
+/*   Updated: 2022/07/20 09:49:26 by aarribas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,17 @@
 
 typedef struct s_stack
 {
-	int				content;
-	struct s_stack	*next;
-}					t_stack;
+	int		*array;
+	size_t	size;
+	size_t	max_size;
+}			t_stack;
 
 typedef struct s_global
 {
-	t_stack			*a;
-	t_stack			*b;
-	t_stack			*head_a;
-	t_stack			*head_b;
-}					t_global;
+	t_stack	*a;
+	t_stack	*b;
+	t_stack	*head_a;
+	t_stack	*head_b;
+}			t_global;
 
 #endif
